4964cd3717e52c540c51732aa65857adb97597b5
Nat! <nat@mulle-kybernetik.com>
2022-01-17 01:54:52 +0100

    fix url

--- mulle-clang-homebrew-test.rb
+++ mulle-clang-homebrew-test.rb
@@ -2,7 +2,7 @@ class MulleClangHomebrewTest < Formula
    homepage "https://github.com/mulle-cc/mulle-clang-homebrew-test"
    desc "Test for: Shim for compiling homebrew packages with the mulle-objc compiler"
 
-   url "https://github.com/mulle-cc/mulle-clang-homebrew-test/archive/refs/tags/0.0.1.tar.gz"
+   url "https://github.com/mulle-cc/mulle-clang-homebrew-test/archive/0.0.1.tar.gz"
    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
    depends_on 'mulle-clang-homebrew' => :build
--- mulle-clang-homebrew.rb
+++ mulle-clang-homebrew.rb
@@ -7,7 +7,7 @@ class MulleClangHomebrew < Formula
   keg_only "this shim is only used inside a brew formula."
 
   # the archive is totally bogus and just used for versioning
-  url "https://github.com/mulle-cc/mulle-clang-homebrew/archive/refs/tags/0.0.2.tar.gz"
+  url "https://github.com/mulle-cc/mulle-clang-homebrew/archive/0.0.2.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   def install
